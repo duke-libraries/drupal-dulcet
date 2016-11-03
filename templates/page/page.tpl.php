5e3c965ffd0c19716097fea936c16fe3427bc297
Sean Aery <sean.aery@duke.edu>
2016-11-03 10:45:39 -0400

    redeveloped footer with Bootstrap3 markup - closes #3

--- templates/page/page.tpl.php
+++ templates/page/page.tpl.php
@@ -72,14 +72,8 @@
           <?php print render($page['content']); ?>
       <?php endif; ?>
     </div>
+
+    <?php require "footer.tpl.php" ?>
+    
   </div> <!-- /#main -->
 </div> <!-- /#main-wrapper -->
-
-<footer id="footer" class="footer" role="footer">
-  <div class="container">
-    <?php if ($copyright): ?>
-      <small class="copyright pull-left"><?php print $copyright; ?></small>
-    <?php endif; ?>
-    <small class="pull-right"><a href="#"><?php print t('Back to Top'); ?></a></small>
-  </div>
-</footer>
