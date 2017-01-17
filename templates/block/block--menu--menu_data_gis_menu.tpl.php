ae8608ac4b83cf7e19f35169d7e102037f5c3c86
Michael Daul <michael.daul@duke.edu>
2017-01-17 10:25:33 -0500

    hard-coded widths for background

--- templates/block/block--menu--menu_data_gis_menu.tpl.php
+++ templates/block/block--menu--menu_data_gis_menu.tpl.php
@@ -6,21 +6,21 @@
       <h2<?php print $title_attributes; ?>><?php print $block->subject; ?></h2>
     <?php endif; ?>
     <?php print render($title_suffix); ?>
-    
+
     <div class="shaded-column-left" <?php print $content_attributes; ?>>
-      
+
       <!-- content above menu -->
             <p class="sidebar-logo"><a href="/data"><img src="<?php print base_path(); ?>sites/default/files/datagis/images/data_gis_logo.png" class="img-thumbnail" alt="image" width="155"></a></p><ul class="nav nav-list"><!--<li class="nav-header nav-header-gray"><a href="/data">Data and GIS Services</a></li>--></ul>
         <!-- / content above menu -->
-      
-      
+
+
       <?php print $content ?>
 
       <div class="sidebar-extras">
         <strong>Contact Us</strong>
         <p class="small">
           <a href="mailto:askdata@duke.edu">askdata@duke.edu</a><br/>
-          <a href="/data/about/lab">Data Visualization Lab</a> (in the Edge)<br/>
+          <a href="/data/about/lab">Data and Visualization Lab</a> (in the Edge)<br/>
           <a href="https://twitter.com/duke_data"><img src="<?php print base_path(); ?>sites/all/themes/madlib/images/icons/twitter24.png" alt=""> @duke_data</a>
         </p>
       </div>
