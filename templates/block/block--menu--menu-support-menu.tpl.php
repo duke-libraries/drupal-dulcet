bf982a8ffbf50743a2d5c6e658c15291a6b63389
Michael Daul <michael.daul@duke.edu>
2017-01-27 13:28:55 -0500

    smaller support button

--- templates/block/block--menu--menu-support-menu.tpl.php
+++ templates/block/block--menu--menu-support-menu.tpl.php
@@ -6,27 +6,27 @@
       <h2<?php print $title_attributes; ?>><?php print $block->subject; ?></h2>
     <?php endif; ?>
     <?php print render($title_suffix); ?>
-    
+
     <div class="shaded-column-left" <?php print $content_attributes; ?>>
-      
+
       <!-- content above menu -->
       <p  class="sidebar-logo">
       <div class="cycle-slideshow">
       <img src="<?php print base_path(); ?>sites/default/files/dul/support/sqfoto5.jpg" alt="Support the Libraries!"> <img src="<?php print base_path(); ?>sites/default/files/dul/support/sqfoto1.jpg" alt="Support the Libraries!"> <img src="<?php print base_path(); ?>sites/default/files/dul/support/sqfoto6.jpg" alt="Support the Libraries!"> <img src="<?php print base_path(); ?>sites/default/files/dul/support/sqfoto4.jpg" alt="Support the Libraries!"> <img src="<?php print base_path(); ?>sites/default/files/dul/support/sqfoto7.jpg" alt="Support the Libraries!"> <img src="<?php print base_path(); ?>sites/default/files/dul/support/sqfoto2.jpg" alt="Support the Libraries!"> <img src="<?php print base_path(); ?>sites/default/files/dul/support/sqfoto3.jpg" alt="Support the Libraries!"> <img src="<?php print base_path(); ?>sites/default/files/dul/support/sqfoto8.jpg" alt="Support the Libraries!">
       </div>
       </p>
-      
+
       <ul class="nav nav-list"><li class="nav-header nav-header-gray"><a href="/support">Support the Libraries</a></li></ul>
-      
+
       <!-- / content above menu -->
-      
-      
+
+
       <?php print $content ?>
 
 
       <div class="sidebar-extras">
         <div style="text-align:center;">
-        <a href="https://www.gifts.duke.edu/library" target="_blank"><button style="width:100%;" class="btn btn-lg btn-primary">Give Now</button></a>
+        <a href="https://www.gifts.duke.edu/library" target="_blank"><button style="width:100%;" class="btn btn-primary">Give Now</button></a>
         </div>
       </div>
 
