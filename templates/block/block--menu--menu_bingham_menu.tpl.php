a91ca04aee8800529eab95bd33f3580af4ed5707
mikedaul <michael.daul@gmail.com>
2017-02-14 20:16:59 +0000

    removed flickr, added twitter

--- templates/block/block--menu--menu_bingham_menu.tpl.php
+++ templates/block/block--menu--menu_bingham_menu.tpl.php
@@ -6,14 +6,14 @@
       <h2<?php print $title_attributes; ?>><?php print $block->subject; ?></h2>
     <?php endif; ?>
     <?php print render($title_suffix); ?>
-    
+
     <div class="shaded-column-left" <?php print $content_attributes; ?>>
-      
+
       <!-- content above menu -->
       <p class="sidebar-logo"><a href="/rubenstein/"> <img class="rl-logo img-thumbnail" src="<?php print base_path(); ?>sites/default/files/rubenstein/rl-logo-white-border-normal-padded-transp.png" class="rl-logo img-thumbnail menu-thumb" title="David M. Rubenstein Rare Book &amp; Manuscript Library"> </a></p><ul class="nav nav-list"><li class="nav-header nav-header-gray"><a href="/rubenstein/bingham/" class="small">Sallie Bingham Center for Women's History &amp; Culture</a></li></ul>
       <!-- / content above menu -->
-      
-      
+
+
       <?php print $content ?>
 
 
@@ -21,7 +21,7 @@
         <strong>Connect With Us</strong>
         <div>
           <a href="https://www.facebook.com/binghamcenter"><img src="<?php print base_path(); ?>sites/all/themes/madlib/images/icons/facebook24.png" alt=""> </a>
-          <a href="http://www.flickr.com/photos/sbcwhc"> <img src="<?php print base_path(); ?>sites/all/themes/madlib/images/icons/flickr24.png" alt=""> </a>
+          <a href="http://twitter.com/binghamctr"> <img src="<?php print base_path(); ?>sites/all/themes/madlib/images/icons/twitter24.png" alt=""> </a>
         </div>
 
         <ul class="nav nav-list list-unstyled"><li class="divider"></li></ul>
