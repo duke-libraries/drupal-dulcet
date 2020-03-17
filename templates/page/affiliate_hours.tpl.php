1152c4d6ed5cbf1fccf0d991caa1d9015b056b67
Michael Daul <michael.daul@duke.edu>
2020-03-17 16:05:26 -0400

    removed covid-19 dukecard blurb (#158)

--- templates/page/affiliate_hours.tpl.php
+++ templates/page/affiliate_hours.tpl.php
@@ -9,18 +9,19 @@
 
 <hr />
 
-<section aria-labelledby="visiting">
+<!-- <section aria-labelledby="visiting"> -->
+	<!--
 	<h2 id="visiting">Visiting the Libraries without a DukeCard</h2>
-
 	<p class="lead">During the Coronavirus response, access to Duke Libraries is limited to members of the Duke University community who will need to swipe their DukeCard of Mobile DukeCard to enter buildings.</p>
-
 	<p>All other visitors &mdash; including TRLN users, Friends of the Libraries, Duke alumni, non-affiliated researchers, and the general public &mdash; <strong>will not have access</strong> until campus restrictions are lifted by the university.</p>
+	-->
 
 	<!--
 	<p class="lead">Members of the public are welcome to use the Duke University Libraries. </p>
 	<p>Most Duke University Libraries are open to the public during typical business hours. When the Libraries are open into the evening, the doors automatically lock after 7pm. After that time, users must swipe their DukeCards to enter the building.</p>
 	-->
-</section>
+
+<!-- </section> -->
 
 <hr />
 
