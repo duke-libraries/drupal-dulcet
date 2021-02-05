61e6ad8f35d22da3f025e5ee733cc1bea69a203a
Zeke Graves <graves.ezekiel@gmail.com>
2021-02-05 15:58:16 -0500

    Update footer.tpl.php (#173)
    Update newsletter subscription URL in footer

--- templates/page/footer.tpl.php
+++ templates/page/footer.tpl.php
@@ -51,7 +51,7 @@
       </div>
 
       <div class="newsletter-signup">
-        <a href="/about/sign-our-newsletter" onClick="ga('send', 'event', { eventCategory: 'Footer', eventAction: 'Social', eventLabel: 'Email List'});">Sign Up for Our Newsletter</a>
+        <a href="/about/newsletter" onClick="ga('send', 'event', { eventCategory: 'Footer', eventAction: 'Social', eventLabel: 'Email List'});">Sign Up for Our Newsletter</a>
       </div>
 
       <ul class="list-unstyled">
